45b0e5ae2758e43fafcf2a49a6f262b5e8e1ae94
woramun wattana <hernfhaja@gmail.com>
2022-06-07 07:08:48 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Whatsapp />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Whatsapp from "./components/whatsapp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Whatsapp,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Whatsapp.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Whats app</h1>
+  </div>
+</template>
